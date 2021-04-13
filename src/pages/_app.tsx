480552fdb493ef3a94a6d70adf3678feb0f2a52d
luiys <luisgustavofeitoza@gmail.com>
2021-04-13 18:36:17 -0300

    Organizando o projeto a partir do boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,11 +7,14 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançadop - Boilerplate</title>
+        <title>My Trips</title>
         <link rel="shortcut icon" href="/images/icon-512.png" />
         <link rel="apple-touch icon" href="/images/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="description" content="Um projeto simples para estudos" />
+        <meta
+          name="description"
+          content="A simple project to show my favorite spots in the world"
+        />
         <meta name="theme-color" content="#06092B" />
       </Head>
       <GlobalStyles />
