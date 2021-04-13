160928d4a0cc56e277d68d3214a3b320782904d5
luiys <luisgustavofeitoza@gmail.com>
2021-04-13 19:50:52 -0300

    Markers Dinamicos

---
+++ src/components/Map/test.tsx
@@ -0,0 +1,39 @@
+import Map from '.'
+import { render, screen } from '@testing-library/react'
+
+describe('<Map />', () => {
+  it('should render without any marker', () => {
+    render(<Map />)
+
+    expect(
+      screen.getByRole('link', { name: /a js library for interactive maps/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render with the marker in correct place', () => {
+    const place = {
+      id: '1',
+      name: 'Petrópolis',
+      slug: 'petropolis',
+      location: {
+        latitude: 0,
+        longitude: 0
+      }
+    }
+
+    const placeTwo = {
+      id: '2',
+      name: 'São Paulo',
+      slug: 'São Paulo',
+      location: {
+        latitude: 20,
+        longitude: 50
+      }
+    }
+
+    render(<Map places={[place, placeTwo]} />)
+
+    expect(screen.getByTitle(/petrópolis/i)).toBeInTheDocument()
+    expect(screen.getByTitle(/São Paulo/i)).toBeInTheDocument()
+  })
+})
