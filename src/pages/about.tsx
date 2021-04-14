c86140d7ca8067cc9ac4d3b866532feac202d1ac
luiys <luisgustavofeitoza@gmail.com>
2021-04-13 22:32:18 -0300

    Request ao CMS ok
    Se buggar com url absoulta ou bugs de css, deve-se fechar e abrir o servidor novamente com o yarn dev

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,16 @@
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
 import AboutTemplate from 'templates/about'
 
-const About = () => <AboutTemplate />
+export default function AboutPage() {
+  return <AboutTemplate />
+}
 
-export default About
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES)
+
+  console.log(pages)
+  return {
+    props: {}
+  }
+}
