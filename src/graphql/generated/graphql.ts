cfd7f57bd0103680e825ae48c6294f840a62a189
luiys <luisgustavofeitoza@gmail.com>
2021-04-14 13:16:21 -0300

    Querys dos places ok
    Faltando apenas colocar links nos markers para ir para a pagina do place

--- src/graphql/generated/graphql.ts
+++ src/graphql/generated/graphql.ts
@@ -2869,12 +2869,37 @@ export type GetPageBySlugQuery = (
   )> }
 );
 
-export type GetPlacesQueryVariables = Exact<{ [key: string]: never; }>;
+export type GetPlacesQueryVariables = Exact<{
+  first?: Maybe<Scalars['Int']>;
+}>;
 
 
 export type GetPlacesQuery = (
   { __typename?: 'Query' }
   & { places: Array<(
+    { __typename?: 'Place' }
+    & Pick<Place, 'id' | 'slug' | 'name'>
+    & { location: (
+      { __typename?: 'Location' }
+      & Pick<Location, 'latitude' | 'longitude'>
+    ), description?: Maybe<(
+      { __typename?: 'RichText' }
+      & Pick<RichText, 'html'>
+    )>, gallery: Array<(
+      { __typename?: 'Asset' }
+      & Pick<Asset, 'url' | 'width' | 'height'>
+    )> }
+  )> }
+);
+
+export type GetPlaceBySlugQueryVariables = Exact<{
+  slug?: Maybe<Scalars['String']>;
+}>;
+
+
+export type GetPlaceBySlugQuery = (
+  { __typename?: 'Query' }
+  & { place?: Maybe<(
     { __typename?: 'Place' }
     & Pick<Place, 'id' | 'slug' | 'name'>
     & { location: (
---
+++ src/templates/Places/index.tsx
@@ -0,0 +1,30 @@
+type ImageProps = {
+  url: string
+  height: number
+  width: number
+}
+
+export type PlacesTemplateProps = {
+  place: {
+    slug: string
+    name: string
+    description: {
+      html: string
+    }
+    gallery: ImageProps[]
+  }
+}
+
+export default function PlacesTemplate({ place }: PlacesTemplateProps) {
+  return (
+    <>
+      <h1>{place.name}</h1>
+
+      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
+
+      {place.gallery.map((image, index) => {
+        return <img src={image.url} alt={place.name} key={index} />
+      })}
+    </>
+  )
+}
