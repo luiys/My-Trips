bcd3d6018acf3510c75e6241323772ff5d5df3c6
luiys <luisgustavofeitoza@gmail.com>
2021-04-16 19:38:29 -0300

    Vercel deploy test

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,6 +1,8 @@
 import { GraphQLClient } from 'graphql-request'
 
-const endpoint = process.env.GRAPHQL_HOST || `${process.env.GRAPHQL_HOST}`
+const endpoint =
+  process.env.GRAPHQL_HOST ||
+  'https://api-ca-central-1.graphcms.com/v2/ckngonac03zl401xoazlz45u7/master'
 
 const client = new GraphQLClient(endpoint, {
   headers: {
