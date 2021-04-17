04de89990607a952fb45ee138d72a3ec6451807a
luiys <luisgustavofeitoza@gmail.com>
2021-04-16 21:26:18 -0300

    Troca de favicon

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,8 +1,6 @@
 import { GraphQLClient } from 'graphql-request'
 
-const endpoint =
-  process.env.GRAPHQL_HOST ||
-  'https://api-ca-central-1.graphcms.com/v2/ckngonac03zl401xoazlz45u7/master'
+const endpoint = process.env.GRAPHQL_HOST || ''
 
 const client = new GraphQLClient(endpoint, {
   headers: {
